6814b9a64ab6754fdde03f803adb448518bdb85c
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2024-10-08 23:09:48 -0400

    feat: add z2m ingress

--- terraform/misc/variables.tf
+++ terraform/misc/variables.tf
@@ -7,3 +7,13 @@ variable "system_username" {
   type        = string
   description = "The username to log in as"
 }
+
+variable "z2m_host" {
+  type        = string
+  description = "The hostname of the Z2M machine"
+}
+
+variable "z2m_url" {
+  type        = string
+  description = "The URL to expose the Z2M ingress at"
+}
