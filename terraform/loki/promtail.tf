57961199c015edf9c0a43203969107e72ec04796
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2025-04-13 21:29:26 -0400

    fix(loki): kill imports after recreating loki cluster

--- terraform/loki/promtail.tf
+++ terraform/loki/promtail.tf
@@ -14,9 +14,3 @@ resource "kubernetes_manifest" "promtail" {
 
   manifest = each.value
 }
-
-import {
-  for_each = local.promtail_manifests
-  to       = kubernetes_manifest.promtail[each.key]
-  id       = "apiVersion=${each.value.apiVersion},kind=${each.value.kind},%{if can(each.value.metadata.namespace)}namespace=${each.value.metadata.namespace},%{endif}name=${each.value.metadata.name}"
-}
